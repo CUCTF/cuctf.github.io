8f4380c66e3e07609b7adb1627a8cd56f872b148
dns43 <dennis.r@columbia.edu>
2019-08-16 18:04:20 -0400

    initial

---
+++ template/partners.js
@@ -0,0 +1,47 @@
+         <section class="about">
+            <h1 align=center><b>Partners</b></h1>
+
+            <!-- WORK AREAS -->
+            <div class="clients">
+                <div class="row">
+                    <!-- client -->
+                    <div class="col-md-3 col-sm-6 client">
+                        <figure>
+                          <a  href="https://www.confiant.com/">
+                            <img src="images/partners/confiant.png">
+                          </a>
+                        </figure>
+                    </div>
+                    <!-- client -->
+                    <div class="col-md-3 col-sm-6 client">
+                        <figure>
+                          <a  href="https://www.redballoonsecurity.com/">
+                            <img src="images/partners/redballoon.png">
+                          </a>
+                        </figure>
+                    </div>
+                    <!-- client -->
+                    <div class="col-md-3 col-sm-6 client">
+                        <figure>
+                          <a  href="https://www.mwrinfosecurity.com/">
+                            <img src="images/partners/mwr.png">
+                          </a>
+                        </figure>
+                    </div>
+                    <!-- client -->
+                    <div class="col-md-3 col-sm-6 client">
+                        <figure>
+                          <a  href="https://www.bnl.gov">
+                            <img src="images/partners/bnl.png">
+                          </a>
+                        </figure>
+                    </div>
+                </div>
+                    </div>
+                  </section>
+            </div>
+        </div>
+        <!-- cont end -->
+
+    </div>
+    <!-- wrapper end -->
